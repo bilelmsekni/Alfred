712be91931931efd011e07a24cb08ddf6d0e83b6
SkiBLE <msekni.bilel@gmail.com>
2016-06-01 21:24:15 +0200

    DEV | Added solution

---
+++ Alfred/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace Alfred
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute("DefaultAPI",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
+        }
+    }
+}
