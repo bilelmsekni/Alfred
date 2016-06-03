283d5b4db2e8003d7b54d84539692d570b5d874e
SkiBLE <msekni.bilel@gmail.com>
2016-06-03 18:17:45 +0200

    DEV | Added a sharedAssemblyInfo

---
+++ Alfred/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("SkiBLE Ltd")]
+[assembly: AssemblyProduct("Alfred")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("SkiBLE Ltd")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
