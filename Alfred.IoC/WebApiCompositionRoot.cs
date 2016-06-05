ca03165e701b7bb029d7989133b196cd5254ef28
SkiBLE <msekni.bilel@gmail.com>
2016-06-05 17:41:51 +0200

    DEV | moved lightInject ioc logic to a seperate project

---
+++ Alfred.IoC/WebApiCompositionRoot.cs
@@ -0,0 +1,16 @@
+﻿using Alfred.Dal.FakeImplementation.Dao;
+using Alfred.Dal.FakeImplementation.Repositories;
+using Alfred.Dal.Interfaces;
+using LightInject;
+
+namespace Alfred.IoC
+{
+    public class WebApiCompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<IMemberRepository, MemberRepository>();
+            serviceRegistry.Register<IMemberDao, MemberDao>();            
+        }
+    }
+}
--- Alfred/Startup.cs
+++ Alfred/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Web.Http;
-using Alfred.Dal.FakeImplementation.Dao;
-using Alfred.Dal.FakeImplementation.Repositories;
-using Alfred.Dal.Interfaces;
+using Alfred.IoC;
 using LightInject;
 using Microsoft.Owin;
 using Newtonsoft.Json;
@@ -20,22 +18,13 @@ namespace Alfred
             var config = new HttpConfiguration();
             var container = new ServiceContainer();
             container.RegisterFrom<WebApiCompositionRoot>();
+            container.RegisterApiControllers();
             container.EnableWebApi(config);          
             config.MapHttpAttributeRoutes();
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             appBuilder.UseWebApi(config);
-        }
-    }
-
-    public class WebApiCompositionRoot : ICompositionRoot
-    {
-        public void Compose(IServiceRegistry serviceRegistry)
-        {
-            serviceRegistry.Register<IMemberRepository, MemberRepository>();
-            serviceRegistry.Register<IMemberDao, MemberDao>();
-            serviceRegistry.RegisterApiControllers(typeof(Startup).Assembly);
         }
     }
 }
