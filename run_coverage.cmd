e61a75ee157376327d36531009561498314fa2c7
SkiBLE <msekni.bilel@gmail.com>
2016-11-11 12:01:58 +0100

    DEV | from debug to release

--- run_coverage.cmd
+++ run_coverage.cmd
@@ -3,5 +3,5 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\Alfred.Tests\bin\Debug\Alfred.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\Alfred.Tests\bin\Release\Alfred.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
