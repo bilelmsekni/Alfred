b1a9f7715e0f13b7d52732a43fe58df9021d490b
SkiBLE <msekni.bilel@gmail.com>
2016-11-11 23:02:24 +0100

    DEV | added quality controller

--- run_quality.cmd
+++ run_quality.cmd
@@ -1,4 +1,4 @@
 choco install "msbuild-sonarqube-runner" -y
-MSBuild.SonarQube.Runner.exe begin /k:"skible:Alfred" /n:"Alfred" /v:"1.0.0" /d:sonar.host.url=https://sonarqube.com /d:sonar.login=SonarQube_Token
+MSBuild.SonarQube.Runner.exe begin /k:"skible:Alfred" /n:"Alfred" /v:"1.0.0" /d:sonar.host.url=https://sonarqube.com /d:sonar.login=[SonarQube_Token]
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild
-MSBuild.SonarQube.Runner.exe end /d:sonar.login=SonarQube_Token
+MSBuild.SonarQube.Runner.exe end /d:sonar.login=[SonarQube_Token]
