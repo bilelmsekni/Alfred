26ab8cde32d1093ea4acd654e45f839da5f21d61
SkiBLE <msekni.bilel@gmail.com>
2016-11-11 12:47:34 +0100

    DEV | enhanced coverage commands

--- run_coverage.cmd
+++ run_coverage.cmd
@@ -3,5 +3,5 @@ nuget install NUnit.Runners -Version 3.4.1 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe -targetargs:".\Alfred.Tests\bin\Release\Alfred.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe -targetargs:".\Alfred.Tests\bin\Release\Alfred.Tests.dll" -filter:"+[Alfred.*]* -[*.Tests]*" -register:user
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
