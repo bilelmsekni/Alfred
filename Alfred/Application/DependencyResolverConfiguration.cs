bdb0f8a798688054b5782466c708dcff916fe83b
SkiBLE <msekni.bilel@gmail.com>
2016-11-14 21:34:57 +0100

    DEV | added Cors middleware

---
+++ Alfred/Application/DependencyResolverConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http.Dependencies;
+
+namespace Alfred.Application
+{
+    public static class DependencyResolverConfiguration
+    {
+        public static T Resolve<T>(this IDependencyResolver resolver)
+        {
+            return (T) resolver.GetService(typeof(T));
+        }
+    }
+}
--- Alfred/Startup.cs
+++ Alfred/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System.Net.Http.Headers;
-using System.Reflection;
-using System.Web.Http;
-using Alfred.IoC;
-using LightInject;
+﻿using System.Web.Http;
+using Alfred.Application;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Owin;
-using Microsoft.Owin.Cors;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using Owin;
-using Swashbuckle.Application;
 
 [assembly: OwinStartup(typeof(Alfred.Startup))]
 
@@ -18,51 +12,14 @@ namespace Alfred
     {
         public void Configuration(IAppBuilder appBuilder)
         {
-            var config = new HttpConfiguration();
+            var config = new HttpConfiguration()
+                .ConfigIoC()
+                .ConfigSwagger()
+                .ConfigWebApi();
 
-            ConfigIoC(config);
-            ConfigSwagger(config);
-            ConfigWebApi(config);
             appBuilder
-                .UseCors(CorsOptions.AllowAll)
+                .UseCors(config.DependencyResolver.Resolve<IConfiguration>())
                 .UseWebApi(config);
-        }
-
-        private void ConfigWebApi(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-        }
-
-        private void ConfigSwagger(HttpConfiguration config)
-        {
-            config.EnableSwagger(c =>
-            {
-                c.SingleApiVersion("v1", GetAssemblyVersion());
-                c.IncludeXmlComments(GetXmlCommentsPath());
-                c.DescribeAllEnumsAsStrings();
-            }).EnableSwaggerUi();
-        }
-
-        private string GetAssemblyVersion()
-        {
-            return Assembly.GetAssembly(typeof(Startup)).GetName().Version.ToString();
-        }
-
-        private void ConfigIoC(HttpConfiguration config)
-        {
-            var container = new ServiceContainer();
-            container.RegisterAssembly(Assembly.GetAssembly(typeof(Startup)));
-            container.RegisterFrom<WebApiCompositionRoot>();
-            container.RegisterApiControllers();
-            container.EnableWebApi(config);
-        }
-
-        private string GetXmlCommentsPath()
-        {
-            return $@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\Alfred.XML";
         }
     }
 }
