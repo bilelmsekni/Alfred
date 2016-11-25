80542bbdcb96e3be0fb80a6c32eadb482064d229
SkiBLE <msekni.bilel@gmail.com>
2016-11-25 22:10:59 +0100

    DEV |  added missing files

---
+++ Alfred.Domain/Models/Artifacts/ArtifactModel.cs
@@ -0,0 +1,23 @@
+using Alfred.Dal.Entities.Enums;
+using Microsoft.Build.Framework;
+
+namespace Alfred.Domain.Models.Artifacts
+{
+    public class ArtifactModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Reward { get; set; }
+        [Required]
+        public ArtifactType Type { get; set; }
+        [Required]
+        public ArtifactStatus Status { get; set; }
+        [Required]
+        public int CommunityId { get; set; }
+        public int MemberId { get; set; }
+        public int Bonus { get; set; }
+    }
+}
---
+++ Alfred.Domain/Models/Artifacts/CreateArtifactModel.cs
@@ -0,0 +1,19 @@
+using Alfred.Dal.Entities.Enums;
+using Microsoft.Build.Framework;
+
+namespace Alfred.Domain.Models.Artifacts
+{
+    public class CreateArtifactModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public ArtifactType Type { get; set; }
+        [Required]
+        public int Reward { get; set; }
+        [Required]
+        public int CommunityId { get; set; }
+        public int Bonus { get; set; }
+        public int MemberId { get; set; }
+    }
+}
