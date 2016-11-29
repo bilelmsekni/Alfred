c2a767a68f5226cee64a2546757e740fc2b75fe7
SkiBLE <msekni.bilel@gmail.com>
2016-11-29 06:58:12 +0100

    DEV | added ignored files

---
+++ Alfred/Models/Artifacts/ArtifactCriteriaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Alfred.Models.Base;
+using Alfred.Shared.Enums;
+
+namespace Alfred.Models.Artifacts
+{
+    public class ArtifactCriteriaModel : BaseCriteriaModel
+    {
+        public IEnumerable<string> Ids { get; set; }
+        public string Title { get; set; }
+        public ArtifactType? Type { get; set; }
+        public ArtifactStatus? Status { get; set; }
+        public int? CommunityId { get; set; }
+        public int? MemberId { get; set; }
+    }
+}
---
+++ Alfred/Models/Artifacts/ArtifactResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Alfred.Models.Base;
+
+namespace Alfred.Models.Artifacts
+{
+    public class ArtifactResponseModel
+    {
+        public IEnumerable<ArtifactModel> Artifacts { get; set; }
+        public IEnumerable<LinkModel> Links { get; set; }
+    }
+}
